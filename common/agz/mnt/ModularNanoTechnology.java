5ea4057dcdd7faa069f27b59502d48252f77430b
AlienGamerZero <julianf@live.nl>
2013-04-23 20:58:37 +0200

    Added some basic files...

---
+++ common/agz/mnt/ModularNanoTechnology.java
@@ -0,0 +1,9 @@
+package agz.mnt;
+
+import agz.mnt.lib.Reference;
+import cpw.mods.fml.common.Mod;
+
+@Mod( modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class ModularNanoTechnology {
+
+}
---
+++ common/agz/mnt/lib/Reference.java
@@ -0,0 +1,8 @@
+package agz.mnt.lib;
+
+public class Reference {
+
+	public static final String MOD_ID = "MNT";
+	public static final String MOD_NAME = "Modular Nano Technology";
+	public static final String VERSION = "0.0.1";
+}
