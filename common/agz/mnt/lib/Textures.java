3203b775eaef1f8c5c0dcff3db8727b6da9e03fb
AlienGamerZero <julianf@live.nl>
2013-04-30 23:16:21 +0200

    Textures

---
+++ common/agz/mnt/lib/Textures.java
@@ -0,0 +1,13 @@
+package agz.mnt.lib;
+
+public class Textures {
+
+	
+	//Base texture folders
+	public static final String BLOCK_LOCATION = "/mods/MNT/textures/blocks/";
+	
+	
+	//Models
+	public static final String BLOCK_NANOFRAME = BLOCK_LOCATION + "BlockNanoFrameTexture.png";
+	
+}
